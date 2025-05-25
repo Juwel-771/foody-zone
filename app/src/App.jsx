27ecfac27b14c910f11d9047e794fc7bb81060e9
Juwel-771 <juwelah99@gmail.com>
2025-05-26 02:02:21 +0600

    food main created

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,22 @@
+import styled from "styled-components"
+
+const MainContainer = styled.div ``
+const TopContainer = styled.div ``
 const App = () => {
-  return <div>App</div>;
+  return(
+     <MainContainer>
+      <TopContainer>
+        <div className="logo">
+          {/* <img src="/logo.png" alt="" /> */}
+          <p>HunGer Me</p>
+        </div>
+
+        <div className="search">
+          <input placeholder="Search Food"/>
+        </div>
+      </TopContainer>
+    </MainContainer>
+  )
 };
 
 export default App;
