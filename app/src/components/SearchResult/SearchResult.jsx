54fc31f48cfda7e6b6fbd7ab0c5b25411ba34d30
Juwel-771 <juwelah99@gmail.com>
2025-05-27 02:17:38 +0600

    food-card created

--- app/src/components/SearchResult/SearchResult.jsx
+++ app/src/components/SearchResult/SearchResult.jsx
@@ -8,15 +8,71 @@ const FoodContainer = styled.section`
 `;
 
 const FoodCards = styled.div``;
-const FoodCard = styled.div``;
+const FoodCard = styled.div`
+  width: 340px;
+  height: 167px;
+  border: 0.66px solid;
 
+  border-image-source: radial-gradient(
+      80.69% 208.78% at 108.28% 112.58%,
+      #eabfff 0%,
+      rgba(135, 38, 183, 0) 100%
+    ),
+    radial-gradient(
+      80.38% 222.5% at -13.75% -12.36%,
+      #98f9ff 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+
+  background: url(.png),
+    radial-gradient(
+      90.16% 143.01% at 15.32% 21.04%,
+      rgba(165, 239, 255, 0.2) 0%,
+      rgba(110, 191, 244, 0.0447917) 77.08%,
+      rgba(70, 144, 213, 0) 100%
+    );
+  background-blend-mode: overlay, normal;
+  backdrop-filter: blur(13.1842px);
+
+  border-radius: 20px;
+
+  display: flex;
+  padding: 8px;
+
+  .food_info {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: end;
+    h3 {
+      margin-top: 8px;
+      font-size: 16px;
+      font-weight: 500;
+    }
+    p {
+      margin-top: 4px;
+      font-size: 12px;
+    }
+    button {
+      font-size: 12px;
+    }
+  }
+`;
+
+const Button = styled.button`
+  background-color: #ff4343;
+  border-radius: 5px;
+  padding: 6px 12px;
+  border: none;
+  color: white;
+`;
 
 const SearchResult = ({data}) => {
   return (
     <FoodContainer>
       <FoodCards>
         {
-            data?.map(({name, image, text})=> (
+            data?.map(({name, image, text, price})=> (
                 <FoodCard key = {name}>
                     <div className="food_image">
                         <img src={BASE_URL + image} />
@@ -26,6 +82,7 @@ const SearchResult = ({data}) => {
                             <h3> {name} </h3>
                             <p> {text} </p>
                         </div>
+                        <Button className="btn">${price}.00</Button>
                     </div>
                 </FoodCard>
             ))
